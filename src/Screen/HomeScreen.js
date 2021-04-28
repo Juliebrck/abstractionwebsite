327235768bd7d73afd5a8a51827e0c2cb6314ed3
Julie Bricka <bricka.j@gmail.com>
2021-04-28 09:32:22 +0200

    #update: change layout card 3

--- src/Screen/HomeScreen.js
+++ src/Screen/HomeScreen.js
@@ -32,7 +32,7 @@ function HomeScreen() {
     backgroundColor: "#1b1a18",
   };
   var card = {
-    height: "72vh",
+    height: "73vh",
     color: "#fffef2",
     border: "none",
     backgroundColor: "#1b1a18",
