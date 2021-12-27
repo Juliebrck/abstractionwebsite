3d0e75ae90070ce75039dd6d3a8a0b71986e8618
Julie Bricka <bricka.j@gmail.com>
2021-12-27 16:53:42 +0100

    add: dropdown menu

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,23 +1,43 @@
 import React, { useState, useEffect } from "react";
 import "../App.css";
 import { connect } from "react-redux";
-import { useHistory } from "react-router-dom";
 import { Link } from 'react-router-dom';
-import { Container, Col } from "reactstrap";
+import { Container, Col, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from "reactstrap";
 import { MenuOutlined, UserOutlined } from '@ant-design/icons';
+
 
 function WelcomeScreen(props) {
 
-  let history = useHistory();
+  const [dropdown, setDropdown] = useState(false);
 
-  const redirect = () => {
-    history.push("/bienvenue");
+  const toggle = () => {
+    setDropdown(!dropdown)
   };
+
 
   return (
     <Container fluid className="App" style={{ paddingLeft: "0px", paddingRight: "0px", marginLeft: 0, marginRight: 0, }}>
       <div style={{ height: '10vh', width: '100vw', padding: "25px", display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-        <MenuOutlined style={{ color: '#fffef2', fontSize: 25 }} />
+
+        <Dropdown isOpen={dropdown} toggle={toggle}>
+          <DropdownToggle style={{ background: "none", border: "none" }} caret>
+            <MenuOutlined style={{ color: '#fffef2', fontSize: 25 }} />
+          </DropdownToggle>
+          <DropdownMenu>
+            <DropdownItem><Link to="/collectionclassique" style={styles.linkmenutop}><h2 style={styles.textmenutop}>COLLECTION CLASSIQUE</h2></Link></DropdownItem>
+            <DropdownItem><Link to="/collectionmillesimee" style={styles.linkmenutop}><h2 style={styles.textmenutop}>COLLECTION MILLÉSIMÉE</h2></Link></DropdownItem>
+            <DropdownItem><Link to="/matierespremieres" style={styles.linkmenutop}><h2 style={styles.textmenutop}>MATIÈRES PREMIÈRES</h2></Link></DropdownItem>
+            <DropdownItem><Link to="/ateliers" style={styles.linkmenutop}><h2 style={styles.textmenutop}>ATELIERS</h2></Link></DropdownItem>
+            <DropdownItem divider />
+            <DropdownItem><Link to="/histoire" style={styles.linkmenutop}><h2 style={styles.textmenutop}>HISTOIRE</h2></Link></DropdownItem>
+            <DropdownItem><Link to="/parfumeurs" style={styles.linkmenutop}><h2 style={styles.textmenutop}>PARFUMEURS</h2></Link></DropdownItem>
+            <DropdownItem><Link to="/partenaires" style={styles.linkmenutop}><h2 style={styles.textmenutop}>PARTENAIRES</h2></Link></DropdownItem>
+            <DropdownItem><Link to="/contact" style={styles.linkmenutop}><h2 style={styles.textmenutop}>CONTACT</h2></Link></DropdownItem>
+
+          </DropdownMenu>
+        </Dropdown>
+
+
         <Link to="/signin" style={styles.linkmenu}><UserOutlined style={{ color: '#fffef2', fontSize: 30 }} /></Link>
       </div>
       <div style={{ height: '80vh', width: '100vw', display: "flex", justifyContent: "space-around", alignItems: "center" }}>
@@ -28,7 +48,7 @@ function WelcomeScreen(props) {
           <Link to="/ateliers" style={styles.linkmenu}><h2 style={styles.textmenu}>ATELIERS</h2></Link>
         </Col>
         <div style={{ width: '40vw', display: "flex", justifyContent: "center", alignItems: "center" }}>
-          <img onClick={() => redirect()} className="logo1" width="30%" src="./logominres.png" alt="logo abstraction" />
+          <img className="logo1" width="30%" src="./logominres.png" alt="logo abstraction" />
           <img className="logo2" width="20%" src="./titremastic.png" alt="logo abstraction" />
         </div>
         <Col className="d-none d-md-block" style={{ width: '30vw' }}>
@@ -70,5 +90,12 @@ const styles = {
   linkmenu: {
     textDecoration: 'none',
     color: "#fffef2",
+  },
+  textmenutop: {
+    fontSize: '14px',
+  },
+  linkmenutop: {
+    textDecoration: 'none',
+    color: "#1b1a18",
   }
 }
